637e9d99d84a16438104adf73ddb31eea0452cc7
Jrdn <83673134+Genbuos@users.noreply.github.com>
2022-03-27 00:09:00 -0400

    added files from my pc

---
+++ 02-backend/Sheco/src/main/java/com/essentialorg/Sheco/ShecoApplication.java
@@ -0,0 +1,13 @@
+package com.essentialorg.Sheco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShecoApplication.class, args);
+	}
+
+}
---
+++ 02-backend/Sheco/src/test/java/com/essentialorg/Sheco/ShecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essentialorg.Sheco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
